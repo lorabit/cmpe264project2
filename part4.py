48abbef7673503d567a576f2f8f6ff7121932ac4
无北 <conglaiwubei@163.com>
2016-12-05 17:35:54 -0800

    fix

--- part4.py
+++ part4.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 img1 = "multiview/p3.JPG"
-points1 = np.array([(952, 1612), (1200, 1540), (1460, 1612), (1444, 1068), (1548, 1160), (2568, 1200), (1548, 1472), (720, 1068), (1028, 1136), (1048, 756), (1648, 736), (1560, 1056), (524, 388), (556, 1240), (544, 696), (136, 1296), (388, 1268), (384, 892)])
-points2 = np.array([(1000, 1812), (1248, 1712), (1464, 1832), (1648, 1052), (1628, 1328), (2728, 1392), (1632, 1624), (1124, 768), (1444, 828), (1456, 440), (2052, 444), (1960, 768), (952, 44), (1012, 884), (988, 348), (368, 1352), (588, 1336), (552, 1004)])
+points2 = np.array([(992, 1784), (1248, 1684), (1464, 1804), (1612, 1340), (1620, 1596), (2696, 1644), (2724, 1384), (368, 1336), (592, 1320), (552, 992), (1128, 764), (1432, 812), (1640, 736), (1948, 752), (2360, 508), (2460, 504), (2620, 784)])
+points1 = np.array([(948, 1588), (1200, 1516), (1452, 1576), (1536, 1180), (1540, 1448), (2560, 1444), (2564, 1184), (144, 1272), (384, 1256), (388, 880), (724, 1056), (1032, 1120), (1252, 1036), (1548, 1036), (1936, 788), (2028, 776), (2184, 1040)])
 
-emtx,emask = cv2.findEssentialMat(points2,points1)
+emtx,emask = cv2.findEssentialMat(points1,points2)
+fmtx,fmask = cv2.findFundamentalMat(points1,points2)
 print("E =")
 print(emtx)
 
@@ -20,9 +21,14 @@ print(VT)
 
 W = np.array([[0,-1,0],[1,0,0],[0,0,1]])
 
-emtx = np.dot(np.dot(U,[[1,0,0],[0,1,0],[0,0,0]]),VT)
+# emtx = np.dot(np.dot(U,[[1,0,0],[0,1,0],[0,0,0]]),VT)
+K = [[2771.214599609375, 0.0, 1387.954468694952], [0.0, 2767.546875, 937.6326279896311], [0.0, 0.0, 1.0]]
+# K = [[2.83973773e+03, 0.00000000e+00, 1.38947081e+03], [0.00000000e+00, 2.83695500e+03, 9.36984169e+02], [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]]
+M = np.linalg.inv(K)
 
-t = U.transpose()[2]
+f = K[0][0]
+
+t = -U.transpose()[2]
 # t = np.matmul(np.matmul(np.matmul(U,W),S),U.transpose())
 R = np.dot(np.dot(U,W),VT)
 
@@ -32,24 +38,54 @@ print("t = ")
 print(t)
 
 rl = VT[2]
-print("rl = ")
-print(rl)
-print("Rrlx - E = ")
-print(np.matmul(R,[[0,-rl[2],rl[1]],[rl[2],0,-rl[0]],[-rl[1],rl[0],0]])-emtx)
+# print("rl = ")
+# print(rl)
+# print("Rrlx - E = ")
+# print(np.matmul(R,[[0,-rl[2],rl[1]],[rl[2],0,-rl[0]],[-rl[1],rl[0],0]])-emtx)
 # print(np.matmul(R.transpose(),R))
 
 
 img=cv2.imread(img1)
 imgplot = plt.imshow(img)
 
+def depth(xl,xr,K,M,R,rr,rl):
+	pr = np.dot(M,bar(xr))
+	pl = np.dot(M,bar(xl))
+	return f*np.dot(f*R[0] - pr[0]*R[2], rl)/np.dot(f*R[0] - pr[0]*R[2], pl)
+
+def bar(v):
+	u = v+ [1]
+	return u
+
+def p2c(pixel, M, f):
+	v = np.dot(M,bar(pixel))
+	v = v*f/v[2]
+	return v
+
 for i in range(len(emask)):
 	if emask[i] == 1:
-		plt.plot([points1[i][0]],points1[i][1],"o",color = 'y')
-		v = np.array([points2[i][0],points2[i][1],1])
-		u = np.matmul(R,v) + t
+		xl = points1[i].tolist()
+		xr = points2[i].tolist()
+		print(bar(xl))
+		z = np.dot(np.dot(bar(xr),fmtx),bar(xl))
+		print("zf = "+str(z))
+		# z = np.dot(np.dot(p2c(xl, M, f),emtx),p2c(xr, M, f))
+		# print("ze = "+str(z))
+		plt.plot(xl[0],xl[1],"o",color = 'y')
+		print("depth = "+str(depth(xl, xr, K, M, R, t, rl)))
+
+
+		# u = np.linalg.inv(R).transpose()
+
+		u = p2c(xr, M, f)
+		print(u)
+		u = np.matmul(R,u) + t
+		print(u)
+		u = np.dot(K, u)
+		print(xl)
+		print(u[0]/u[2],u[1]/u[2])
 		plt.plot([u[0]/u[2]],[u[1]/u[2]],"x",color = 'r')
-		print([u[0]/u[2],u[1]/u[2]])
 
 
 plt.autoscale(enable=True, axis='both', tight=True)
-plt.show()
+# plt.show()
--- undistort.py
+++ undistort.py
@@ -8,8 +8,9 @@ dist = np.array([[-0.12244297, 0.20499093, 0.00035033, -0.00058871, -0.13275741]
 h,  w = 1856, 2784
 newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
+
 print(mtx)
-print(newcameramtx)
+print(newcameramtx.tolist())
 
 files = os.listdir('multiview')
 for filename in files:
@@ -17,5 +18,6 @@ for filename in files:
 		img = cv2.imread('multiview/'+filename)
 		dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
 		x,y,w,h = roi
+		print(roi)
 		dst = dst[y:y+h, x:x+w]
 		cv2.imwrite('undistorted/'+filename,dst)
